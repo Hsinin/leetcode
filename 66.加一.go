7b023bb7366ea28d7fb587b479ff35d769d5195c
xin_wang <wangxin16@asiainfo-sec.com>
2021-01-27 10:01:39 +0800

    66: 加一

--- 66.加一.go
+++ 66.加一.go
@@ -3,17 +3,37 @@
  *
  * [66] 加一
  */
+package main
+
+import "fmt"
 
 // @lc code=start
 func plusOne(digits []int) []int {
-	a := [len(digits) + 1]int{}
+	// a := [len(digits) + 1]int{}
+	a := make([]int, len(digits)+1)
+	flag := 0
 	digits[len(digits)-1]++
 	for i := len(digits) - 1; i >= 0; i-- {
 		a[i+1] = digits[i] % 10
 		if digits[i] > 9 {
-			a[i]++
+			if i > 0 {
+				digits[i-1]++
+			} else {
+				a[0] = 1
+				flag = 1
+			}
 		}
+	}
+	if flag == 1 {
+		return a
+	} else {
+		return a[1:]
 	}
 }
 
 // @lc code=end
+func main() {
+	// arrayList := []int{1, 2, 3, 4}
+	arrayList := []int{9, 9, 9}
+	fmt.Println(plusOne(arrayList))
+}
